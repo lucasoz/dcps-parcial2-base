76823facf1475918fca8ab3f936f728f69253650
lucasoz <luemunozmo@unal.edu.co>
2018-05-11 11:50:35 -0500

    terminado

---
+++ app/controllers/authors/author.js
@@ -0,0 +1,15 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    delete(post){
+      const answer = confirm("Are you sure delete this post?");
+      if (answer) {
+        post.deleteRecord();
+        post.get('isDeleted'); // => true
+        post.save(); //
+        
+      }
+    }
+  }
+});
---
+++ app/models/author.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  username: DS.attr('string'),
+  email: DS.attr('string'),
+  profilePicture: DS.attr('string'),
+
+  posts: DS.hasMany('blog-post', { async: true }),
+
+});
